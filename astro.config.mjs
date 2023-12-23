a8cb6838f879fdbf4883efb6190e2f7b214e1e28
Oliver Williams <oliverwilliams345@gmail.com>
2023-12-23 13:53:52 +0000

    update config file with the website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://fullystacked.net',
 	integrations: [mdx(), sitemap()],
 	redirects: {
 		'/posts/[...slug]': '/[...slug]'
