271f256e571ee5a1f93ad49204f1c92280d435da
Oliver Williams <oliverwilliams345@gmail.com>
2023-12-23 15:42:46 +0000

    remove font preloading. Updated site title for head

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Fully Stacked';
+export const SITE_DESCRIPTION = 'A blog about web standards, modern CSS, HTML and JavaScript';
