f99015d53f4e724200043d8f9e7e69066004aaa3
Oliver Williams <oliverwilliams345@gmail.com>
2025-12-29 11:38:46 +0000

    possibly fix rss feed?...

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/${post.slug}/`,
+			link: `/blog/${post.slug}/`,
 		})),
 	});
 }
